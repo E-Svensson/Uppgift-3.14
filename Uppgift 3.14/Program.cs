c5fae081e6ef12057b17997bd6977bf99caf2d53
AdamAkhooo <adamjamal0505@gmail.com>
2022-09-30 10:23:10 +0200

    Add project files.

---
+++ Uppgift 3.14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uppgift_314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? (JA eller NEJ)");
+            Console.Write("Svara här: "); string svar1 = Console.ReadLine().ToLower();
+            Console.WriteLine("Hur gammal är du?");
+            Console.Write("Svara här: "); int svar2 = int.Parse(Console.ReadLine());
+
+            char[] svar = svar1.ToCharArray();
+
+            if (svar[0] == 'j' && svar2 < 22)
+            {
+                Console.WriteLine("Vi vil gärna anställa sig.");
+            }
+
+            else
+            {
+                Console.WriteLine("Vi leter tyvärr efter annan personal just nu.");
+            }
+        }
+    }
+}
